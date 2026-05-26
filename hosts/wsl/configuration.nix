f6b6571b8463234694ee2cdd43a16c7b3dfe2cf2
Mathod <felix.mathias95@gmail.com>
2026-05-26 02:48:47 +0200

    updated configuration.nix to include the latest changes for WSL setup.

--- hosts/wsl/configuration.nix
+++ hosts/wsl/configuration.nix
@@ -8,29 +8,6 @@
 { config, lib, pkgs, ... }:
 
 {
-nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-#  imports = [
-#    # include NixOS-WSL modules
-#    <nixos-wsl/modules>
-#  ];
-
-  wsl.enable = true;
-  wsl.defaultUser = "mathod";
-
-  users.users.mathod = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" ];
-  };
-
-  # Mathod
-  nixpkgs.config.allowUnfree = true;
-  programs.nix-ld.enable = true;
-  environment.systemPackages = with pkgs; [
-    vscode
-    wget
-  ];
-
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It's perfectly fine and recommended to leave
@@ -38,4 +15,28 @@ nix.settings.experimental-features = [ "nix-command" "flakes" ];
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.11"; # Did you read the comment?
+
+  # Mathod
+
+  # Enable experimental features
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.mathod = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+  };
+
+  # Enable WSL support
+  wsl.enable = true;
+  wsl.defaultUser = "mathod";
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  programs.nix-ld.enable = true;
+  environment.systemPackages = with pkgs; [
+    vscode
+    wget
+  ];
 }
